17b8a46cac76621eb9eb5cad7df38f64919083bc
Nabeel Virani <nabeelvirani@gmail.com>
2016-05-09 22:12:10 -0500

    added oauth modules and ui modules/carousel

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
-var app = angular.module('app', ['ngRoutes']);
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap', "ngAnimate"]);
 
 
-app.controller("MainController", ['$scope', function("$scope"){
+app.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when("/", {
+			templateUrl: "pages/home.html",
+			controller: "MainController"
+		})
+		.when("/about", {
+			templateUrl: "pages/about.html",
+			controller: "AboutController"
+		})
+		.when("/contact", {
+			templateUrl: "pages/contact.html",
+			controller: "ContactController",
+			requireToken: true
+		})
+		.otherwise({
+			redirectTo: "pages/home.html"
+		});
+		$locationProvider.html5Mode(true)
+});	
+
+
+
+app.controller("MainController", ['$scope', function($scope){
 		$scope.message = "This is my Angular app."
+
+}]);
+
+
+
+app.controller("AboutController", ['$scope', function($scope){
+		$scope.message = "This is about me."
+}]);
+
+
+app.controller("ContactController", ['$scope', function($scope){
+		$scope.message = "This is how to contact me."
 }]);
