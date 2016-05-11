2ec975e2cb6ae914671a811ac5667fbde811c579
Nabeel Virani <nabeelvirani@gmail.com>
2016-05-10 21:13:54 -0500

    added a controller for auth, adding feature for new tab during auth

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var app = angular.module('app', ['ngRoute', 'ui.bootstrap', "ngAnimate"]);
 
 
+
+
+OAuth.initialize('DwPepa-DHxsmZP-R6ytDR3ls5ZU')
+
 app.config(function($routeProvider, $locationProvider){
 	$routeProvider
 		.when("/", {
@@ -16,8 +20,11 @@ app.config(function($routeProvider, $locationProvider){
 			controller: "ContactController",
 			requireToken: true
 		})
+		.when("/access_token=:access_token",{
+			controller: 'LoginController'
+		})
 		.otherwise({
-			redirectTo: "pages/home.html"
+			redirectTo: "/"		
 		});
 		$locationProvider.html5Mode(true)
 });	
@@ -38,4 +45,18 @@ app.controller("AboutController", ['$scope', function($scope){
 
 app.controller("ContactController", ['$scope', function($scope){
 		$scope.message = "This is how to contact me."
+}]);
+
+
+app.controller("LoginController", ['$scope', "$routeParams", "$window", function($scope, $routeParams, $window){
+		console.log($routeParams)
+		var $parentscope = $window.opener.angular.element(window.opener.document).scope();
+		if(angular.isDefined($routeParams.access_token)) {
+    		parentscope.$broadcast("igAccessTokenObtained",
+     			{ access_token: $routeParams.access_token
+     		})
+   		}
+   	console.log("hello")
+    $window.close();
+    console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
 }]);
--- carousel.js
+++ carousel.js
@@ -1,13 +1,13 @@
 
 var app = angular.module("app");
 
-app.controller('CarouselCtrl', function ($scope) {
+app.controller('CarouselCtrl', function ($scope, $http, $window, $location) {
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
   $scope.active = 0;
+  var id = '10uki80x2gckzal'
   var slides = $scope.slides = [];
   var currIndex = 0;
-
   $scope.addSlide = function() {
     var newWidth = 600 + slides.length + 1;
     slides.push({
@@ -15,6 +15,37 @@ app.controller('CarouselCtrl', function ($scope) {
       id: currIndex++
     });
   };
+  $scope.generateCSRF = function(){
+    return crypto.randomBytes(18).toString('base64')
+    .replace(/\//g, '-').replace(/\+/g, '_');
+  }
+
+
+  $scope.acquire_code = function(){
+   // https://www.dropbox.com/1/oauth2/authorize?client_id=<app key>&response_type=code&redirect_uri=<redirect URI>&state=<CSRF token>
+    $http.jsonp("https://api.dropboxapi.com/1/oauth2/authorize?response_type=code", {
+      method: "POST"
+    }).success(function(data, status, headers, config){
+      console.log("here are the responses")
+      console.log(data)
+      console.log(status)
+      console.log(headers)
+      console.log(config)
+    }).error(function(data, status, headers, config){
+      console.log(data)
+      console.log(config)
+    })
+  }
+  $scope.auth = function() {
+    console.log($window.open('https://www.dropbox.com/1/oauth2/authorize?client_id=10uki80x2gckzal&response_type=token&redirect_uri=http://127.0.0.1:1121'));
+    //code = $scope.acquire_code();
+   /* $http({
+        method:"GET",
+        url: 'https://www.dropbox.com/1/oauth2/authorize'//?client_id=<app key>&response_type=token&redirect_uri=<redirect URI>&state=<CSRF token>',
+        grant_type: 'authorization_code'
+    })
+*/
+  }
 
   $scope.randomize = function() {
     var indexes = generateIndexesArray();
